7df42cdaceaefba27992a73f17986704b0c89fbc
shreyaa <shetshreyaa25@gmail.com>
2022-01-11 22:27:02 +0530

    style:add sidebar and navbar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,75 +1,104 @@
 import styled from "styled-components";
 // hve the link from both router n react-scroll
-import {Link as LinkR} from 'react-router-dom';
-import {Link as LinkS} from 'react-scroll';
+import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS } from "react-scroll";
 
- export const Nav= styled.nav`
- background:#000;
- height:80px;
- align-items:center;
- display:flex;
- font-size:1rem;
- justify-content:center;
- position:sticky;
- top:0;
- z-index:10;
+export const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  align-items: center;
+  display: flex;
+  font-size: 1rem;
+  justify-content: center;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 
- @media screen and (max-width:960px){
-     transition:0.8s all ease;
- }`;
-
- export const NavbarContainer= styled.div`
- display:flex;
- justify-content:space-between;
- height:80px;
- z-index:1;
- width:100%;
- padding:0 24px;
- max-width:1100px;
-`;
-export const NavLogo=styled(LinkR)`
-color:#fff;
-justify-self:flex-start;
-cursor:pointer;
-font-size:1.5rem;
-display:flex;
-align-items:center;
-margin-left:2px;
-font-weight:bold;
-text-decoration:none;
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
 `;
 
-export const MobileIcons=styled.div`
-display:none;
-@media screen and (max-width:760px){
-  display:block;
-  position:absolute;
-  top:0;
-  right:0;
-  transform:translate(-100%,-60%);
-  font-size:1.8rem;
-  cursor:pointer;
-}`;
-export const NavMenu=styled.ul`
-display:flex;
-align-items:center;
-list-style:none;
-text-align:center;
-margin-right:-2px;
-@media screen and (max-width:760px){
-    display:none;
-}`;
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1100px;
+`;
+export const NavLogo = styled(LinkR)`
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-left: 2px;
+  font-weight: bold;
+  text-decoration: none;
+`;
+
+export const MobileIcons = styled.div`
+  display: none;
+  @media screen and (max-width: 760px) {
+    display: block;
+    position: absolute;
+    top: 60%;
+    right: 0;
+    transform: translate(-100%, -60%);
+    font-size: 1.8rem;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  margin-right: -2px;
+  @media screen and (max-width: 760px) {
+    display: none;
+  }
+`;
 export const NavbarItems = styled.li`
-height:80px;`;
+  height: 80px;
+`;
 
-export const NavLinks=styled(LinkS)`
-color:#fff;
-display:flex;
-align-items:center;
-text-decoration:none;
-padding:0 1rem;
-height:100%;
-cursor:pointer;
-&.active{
-    border-bottom:3px solid #01bf71;
-}`;
+export const NavLinks = styled(LinkS)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    border-bottom: 3px solid #01bf71;
+  }
+`;
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  @media screen and (max-width: 760px) {
+    display: none;
+  }
+`;
+export const NavBtnLink = styled(LinkR)`
+  border-radius: 5px;
+  background: #010606;
+  color: #01bf71;
+  white-space: nowrap;
+  padding: 10px 15px;
+  font-size: 1.3rem;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #01bf71;
+    color: #010606;
+  }
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo,MobileIcons,NavMenu,NavbarItems,NavLinks
+import { Nav, NavbarContainer, NavLogo,MobileIcons,NavMenu,NavbarItems,NavLinks,NavBtn,NavBtnLink
  } from "./NavbarElements";
 import { FaBars } from "react-icons/fa";
 
@@ -16,6 +16,18 @@ const Navbar = () => {
               <NavbarItems>
                   <NavLinks to='about'>About</NavLinks>
                 </NavbarItems>
+                <NavbarItems>
+                  <NavLinks to='discover'>Discover</NavLinks>
+                </NavbarItems>
+                <NavbarItems>
+                  <NavLinks to='services'>Services</NavLinks>
+                </NavbarItems>
+                <NavbarItems>
+                  <NavLinks to='signUp'>Sign-up</NavLinks>
+                </NavbarItems>
+                <NavBtn>
+                  <NavBtnLink to='signIn'>Sign-in</NavBtnLink>
+                </NavBtn>
           </NavMenu>
 
         </NavbarContainer>
